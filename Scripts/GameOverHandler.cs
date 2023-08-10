8d929fe85baf23646fded9d5ee86ebe74a51a520
Kostas Straplakis <116097306+kostasstrap@users.noreply.github.com>
2023-08-10 18:51:23 +0300

    Add files via upload

---
+++ Scripts/GameOverHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_Text gameOverText;
+    [SerializeField] private ScoreSystem scoreSystem;
+    [SerializeField] private GameObject gameOverDisplay;
+    [SerializeField] private AsteroidSpawner asteroidSpawner;
+    public void EndTheGame()
+    {
+        asteroidSpawner.enabled = false;
+        
+        scoreSystem.gameObject.SetActive(false);
+        
+        int finalScore = scoreSystem.EndTimer();
+
+        gameOverText.text = $"Your Score: {finalScore}";
+
+        gameOverDisplay.gameObject.SetActive(true);
+
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
